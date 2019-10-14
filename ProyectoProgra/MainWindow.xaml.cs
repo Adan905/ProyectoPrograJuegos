b04e02785ef69affec8282a06702924504e50870
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-10-14 13:00:17 -0700

    Agregar archivos de proyecto.

---
+++ ProyectoProgra/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProyectoProgra
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BnAgregar_Click(object sender, RoutedEventArgs e)
+        {
+            switch (Consolas.SelectedIndex)
+            {
+                case 0:
+                    Parametros.Children.Add(new ParNin64());
+                    break;
+                case 1:
+                    Parametros.Children.Add(new ParGamecube());
+                    break;
+                case 2:
+                    Parametros.Children.Add(new ParPlay2());
+                    break;
+                case 3:
+                    Parametros.Children.Add(new ParXbox ());
+                    break;
+                case 4:
+                    Parametros.Children.Add(new ParWii());
+                    break;
+                case 5:
+                    Parametros.Children.Add(new ParPlay2());
+                    break;
+            }
+        }
+    }
+}
---
+++ ProyectoProgra/ParPlay.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProyectoProgra
+{
+    /// <summary>
+    /// Lógica de interacción para ParPlay.xaml
+    /// </summary>
+    public partial class ParPlay : UserControl
+    {
+        public ParPlay()
+        {
+            InitializeComponent();
+        }
+
+        private void BnGuardar_Click(object sender, RoutedEventArgs e)
+        {
+            if (NombrePro.Text == "" || Uso.Text == "" || Valor.Text == "" || Descuento.Text == "" || Antigüedad.Text == "")
+                Señal.Visibility = Visibility.Visible;
+
+            else
+                n64.Visibility = Visibility.Hidden;
+        }
+
+        private void BnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+            n64.Visibility = Visibility.Hidden;
+        }
+
+        private void Descuento_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Señal.Visibility = Visibility.Hidden;
+        }
+
+        private void Uso_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Señal.Visibility = Visibility.Hidden;
+        }
+
+        private void Valor_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Señal.Visibility = Visibility.Hidden;
+        }
+
+        private void NombrePro_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Señal.Visibility = Visibility.Hidden;
+        }
+
+        private void Antigüedad_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Señal.Visibility = Visibility.Hidden;
+        }
+
+    }
+}
